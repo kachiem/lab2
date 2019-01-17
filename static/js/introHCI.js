6bd1ea3c2ea75f91b20e75b77fd3acfa561dbcb3
kachiem <kachiem@ucsd.edu>
2019-01-16 22:14:52 -0800

    added css, bootstrap, js and jquery functions and listeners

---
+++ static/js/introHCI.js
@@ -0,0 +1,12 @@
+function initializePage() {
+	$("#testjs").click(function(e) {
+		$('jumbotron h1').text("Javascript is connected");
+	});
+
+	$("a.thumbnail").click(projectClick);
+}
+
+function projectClick() {
+	e.preventDefault();
+	$(this).css("background-color", "#7fff00");
+}
